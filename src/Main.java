ff66d82c2b9563658fb892a130fd2a39e027bf5e
SamuelB0X <samueloviedo397@gmail.com>
2025-03-10 19:06:32 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        PrimeDirective pd = new PrimeDirective();
+        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+
+        System.out.println(pd.isPrime(7));
+        System.out.println(pd.isPrime(28));
+        System.out.println(pd.isPrime(2));
+        System.out.println(pd.isPrime(0));
+
+        System.out.println(pd.onlyPrimes(numbers));
+    }
 }
--- src/PrimeDirective.java
+++ src/PrimeDirective.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
+import java.util.ArrayList;
 public class PrimeDirective {
+
+    public boolean isPrime(int number) {
+        if (number == 2) {
+            return true;
+        } else if (number < 2) {
+            return false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> onlyPrimes(int[] numbers) {
+        ArrayList<Integer> primes = new ArrayList<>();
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                primes.add(number);
+            }
+        }
+        return primes;
+    }
 }
+
